a46ea27358f34fe37f8913ad7e263aba31bee4a7
mohamed nkaies <mohamednkaies3@gmail.com>
2022-11-21 09:15:53 +0100

    add swagger

--- TestFivePoints/src/main/java/com/example/TestFivePoints/Configuration/SecurityConfiguration.java
+++ TestFivePoints/src/main/java/com/example/TestFivePoints/Configuration/SecurityConfiguration.java
@@ -35,11 +35,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/login" , "/client/register")
+        http.csrf().disable().authorizeRequests().antMatchers("/login" , "/client/register","/v3/api-docs" )
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        
+
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
